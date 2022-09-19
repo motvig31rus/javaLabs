963f0899dffbdc30fbc0f6cd1d80fe51dbe07dcb
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-09-19 18:40:46 +0300

    Zadachi Dogruzka

---
+++ zd9.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class zd9 {
+    public static void main(String[] arg) {
+        ninthTask();
+    }
+    public static void ninthTask() {
+            int[] numbers = {15,10,51, 6, 5,3, 10,-34,0,32,56, 12,24, 52};
+            for (int i = 0; i < numbers.length / 2; i++) {
+                int temp = numbers[i];
+                numbers[i] = numbers[numbers.length - 1 - i];
+                numbers[numbers.length - 1 - i] = temp;
+            }
+            System.out.print("Перевернутый массив: " + Arrays.toString(numbers));
+        }
+    }
